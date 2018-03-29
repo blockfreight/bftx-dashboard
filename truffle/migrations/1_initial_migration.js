bb792ff9d4fc6d4ec62bb24746291d5c99cde301
arsen <arsen3d@gmail.com>
2018-03-29 08:46:07 -0700

    centered via bootstrap

--- truffle/migrations/1_initial_migration.js
+++ truffle/migrations/1_initial_migration.js
@@ -3,3 +3,15 @@ var Migrations = artifacts.require("./Migrations.sol");
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
 };
+var ProofOfExistence = artifacts.require("./ProofOfExistence.sol")
+var Ownable = artifacts.require("./Ownable.sol")
+var RBAC = artifacts.require("./RBAC.sol")
+var Roles = artifacts.require("./Roles.sol")
+var Whitelist = artifacts.require("./Whitelist.sol")
+module.exports = function(deployer) {
+    deployer.deploy(ProofOfExistence);
+    deployer.deploy(Ownable);
+    deployer.deploy(RBAC);
+    deployer.deploy(Roles);
+    deployer.deploy(ProofOfExistence);
+}Whitelist
